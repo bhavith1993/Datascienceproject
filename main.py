cfd38046ada1c4760591f34a7c57e72507a657f0
Bhavith Shetty <bhavithnkr@gmail.com>
2025-01-27 12:47:24 -0500

    Data Transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.Datascienceproject import logger
 from src.Datascienceproject.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from src.Datascienceproject.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.Datascienceproject.pipeline.data_transformation import DataTransformationTrainingPipeline
 
 
 
@@ -26,3 +27,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataTransformationTrainingPipeline()
+    data_ingestion.initiate_data_transformation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
--- src/Datascienceproject/entity/config_entity.py
+++ src/Datascienceproject/entity/config_entity.py
@@ -16,3 +16,7 @@ class DataValidationConfig:
     unzip_data_dir:Path
     all_schema:dict
 
+@dataclass
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
